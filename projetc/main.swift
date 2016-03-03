dd389ac4877f917e42532cac05028d5ac6d494ea
20131105787 <anshuang@mac05.local>
2016-03-03 10:29:31 +0800

    Initial Commit

---
+++ projetc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  projetc
+//
+//  Created by 20131105787 on 16/3/3.
+//  Copyright © 2016年 20131105787. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
